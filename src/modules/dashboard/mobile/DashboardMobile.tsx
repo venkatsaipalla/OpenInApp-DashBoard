e4370dddb2b041b3cd18933954abc81761ea0a4c
venkatsaipalla <venkatasaipalla2019@email.com>
2024-02-02 01:01:46 +0530

    file changes

--- src/modules/dashboard/mobile/DashboardMobile.tsx
+++ src/modules/dashboard/mobile/DashboardMobile.tsx
@@ -9,7 +9,8 @@ import { IoIosNotifications } from "react-icons/io";
 import { FaCalendarAlt, FaCloudUploadAlt, FaRegBell } from "react-icons/fa";
 import { HiMiniTicket } from "react-icons/hi2";
 import { LuUpload } from "react-icons/lu";
-
+import readXlsxFile from "read-excel-file";
+import * as XLSX from "xlsx";
 const MenuTabs = [
   {
     id: 1,
@@ -75,21 +76,61 @@ const MobileMenu = (props: any) => {
 };
 
 export default function DashboardMobile() {
+  const [data, setData] = useState([]);
+
   const [selectedFile, setSelectedFile] = useState(null);
+  const [fileName, setFileName] = useState(null);
   const [activeTab, setActiveTab] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
+  const [error, setError] = useState("");
+  const allowedExtensions = ["csv", "xls", "xlsx"];
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
+  console.log({ error });
+  console.log({ data });
+  const handleFileChange = (event: any) => {
+    console.log(event);
+    if (event.target.files[0]) {
+      setError("");
+      const fileExtension = event.target.files[0]?.name.split(".")[1];
+      console.log(fileExtension);
+      if (!allowedExtensions.includes(fileExtension)) {
+        setError("Please input a csv file");
+        return;
+      }
+      setFileName(event.target.files[0].name);
+      setSelectedFile(event.target.files[0]);
+    }
   };
   const handleSubmit = (event: { preventDefault: () => void }) => {
     event.preventDefault();
     // Handle the file submission here, you can send it to the server or process it further.
     console.log("Selected file:", selectedFile);
+    readCSVFile();
   };
-  // const drawerProps = { close: toggleMenu };
+  const removeFile = () => {
+    setSelectedFile(null);
+    setFileName(null);
+  };
+  const readCSVFile = () => {
+    selectedFile &&
+      readXlsxFile(selectedFile).then((rows) => {
+        console.log(rows);
+      });
+  };
+  const handleFileUpload = (e: any) => {
+    const reader = new FileReader();
+    reader.readAsBinaryString(e.target.files[0]);
+    reader.onload = (e) => {
+      const data = e.target.result;
+      const workbook = XLSX.read(data, { type: "binary" });
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      const parsedData = XLSX.utils.sheet_to_json(sheet);
+      setData(parsedData);
+    };
+  };
   return (
     <CVFlex className={`${DashboardMobileStyles.bgContainer}`}>
       {isOpen && (
@@ -139,18 +180,38 @@ export default function DashboardMobile() {
               <input
                 id="upload"
                 type="file"
-                onChange={handleFileChange}
+                // onChange={handleFileChange}
+                onChange={handleFileUpload}
                 style={{ display: "none" }}
+                accept=".csv,.xlsx,.xls"
               />
-              <p className={`${DashboardMobileStyles.secondaryText}`}>
-                Drop your excel sheet here or{" "}
-                <span style={{ color: "#605BFF" }}>browse</span>
-              </p>
+              {selectedFile ? (
+                <p
+                  className={`${DashboardMobileStyles.secondaryText}`}
+                  style={{ textOverflow: "ellipsis" }}
+                >
+                  {fileName}
+                  <br />
+                  <span
+                    style={{ color: "red", cursor: "pointer" }}
+                    onClick={removeFile}
+                  >
+                    remove
+                  </span>
+                </p>
+              ) : (
+                <p className={`${DashboardMobileStyles.secondaryText}`}>
+                  Drop your excel sheet here or{" "}
+                  <span style={{ color: "#605BFF" }}>browse</span>
+                </p>
+              )}
             </CVFlex>
           </label>
           <button
             className={`${DashboardMobileStyles.submitButton}`}
             onClick={handleSubmit}
+            disabled={fileName !== null ? false : true}
+            style={fileName !== null ? { opacity: "100%" } : { opacity: "40%" }}
           >
             {/* <CHFlex className={`${DashboardMobileStyles.submitButton}`}> */}
             <LuUpload className="inherit" />
@@ -184,103 +245,82 @@ export default function DashboardMobile() {
             </p>
           </CHFlex>
           <ul className={`${DashboardMobileStyles.orderList}`}>
-            <li className={`${DashboardMobileStyles.listItem}`}>
-              <p className={`${DashboardMobileStyles.listItemText}`}>01</p>
-              <a
-                className={`${DashboardMobileStyles.listItemText}`}
-                style={{ color: "#5B93FF" }}
-              >
-                www.google.com
-              </a>
-              <p className={`${DashboardMobileStyles.listItemText}`}>
-                prefixsample
-              </p>
-              <div className={`${DashboardMobileStyles.custom_select_Div}`}>
-                <select className={`${DashboardMobileStyles.selectTag}`}>
-                  <option value="1">Tag 1</option>
-                  <option value="2">Tag 2</option>
-                  <option value="3">Tag 3</option>
-                  <option value="4">Tag 4</option>
-                  <option value="5">Tag 5</option>
-                </select>
-              </div>
-              <CHFlex className={`${DashboardMobileStyles.selectedTagDiv}`}>
-                <CHFlex className={`${DashboardMobileStyles.selectedTags}`}>
-                  <p className={`${DashboardMobileStyles.selectTagText}`}>
-                    TAG 1
+            {data &&
+              data.map((item: any) => (
+                <li
+                  className={`${DashboardMobileStyles.listItem}`}
+                  key={item.id}
+                >
+                  <p className={`${DashboardMobileStyles.listItemText}`}>
+                    {item.id}
                   </p>
-                  <RxCross2 className={`${DashboardMobileStyles.cross}`} />
-                </CHFlex>
-                <CHFlex className={`${DashboardMobileStyles.selectedTags}`}>
-                  <p className={`${DashboardMobileStyles.selectTagText}`}>
-                    TAG 1
+                  <a
+                    className={`${DashboardMobileStyles.listItemText}`}
+                    style={{ color: "#5B93FF" }}
+                  >
+                    {item.links}
+                  </a>
+                  <p className={`${DashboardMobileStyles.listItemText}`}>
+                    {item.prefix}
                   </p>
-                  <RxCross2 className={`${DashboardMobileStyles.cross}`} />
-                </CHFlex>
-                <CHFlex className={`${DashboardMobileStyles.selectedTags}`}>
-                  <p className={`${DashboardMobileStyles.selectTagText}`}>
-                    TAG 1
-                  </p>
-                  <RxCross2 className={`${DashboardMobileStyles.cross}`} />
-                </CHFlex>
-                <CHFlex className={`${DashboardMobileStyles.selectedTags}`}>
-                  <p className={`${DashboardMobileStyles.selectTagText}`}>
-                    TAG 1
-                  </p>
-                  <RxCross2 className={`${DashboardMobileStyles.cross}`} />
-                </CHFlex>
-              </CHFlex>
-            </li>
-            <li className={`${DashboardMobileStyles.listItem}`}>
-              <p className={`${DashboardMobileStyles.listItemText}`}>01</p>
-              <a
-                className={`${DashboardMobileStyles.listItemText}`}
-                style={{ color: "#5B93FF" }}
-              >
-                www.google.com
-              </a>
-              <p className={`${DashboardMobileStyles.listItemText}`}>
-                prefixsample
-              </p>
-              <div className={`${DashboardMobileStyles.custom_select_Div}`}>
-                <select className={`${DashboardMobileStyles.selectTag}`}>
-                  <option value="1">Tag 1</option>
-                  <option value="2">Tag 2</option>
-                  <option value="3">Tag 3</option>
-                  <option value="4">Tag 4</option>
-                  <option value="5">Tag 5</option>
-                </select>
-              </div>
-              <CHFlex className={`${DashboardMobileStyles.selectedTagDiv}`}>
-                <CHFlex className={`${DashboardMobileStyles.selectedTags}`}>
-                  <p className={`${DashboardMobileStyles.selectTagText}`}>
-                    TAG 1
-                  </p>
-                  <RxCross2 className={`${DashboardMobileStyles.cross}`} />
-                </CHFlex>
-                <CHFlex className={`${DashboardMobileStyles.selectedTags}`}>
-                  <p className={`${DashboardMobileStyles.selectTagText}`}>
-                    TAG 1
-                  </p>
-                  <RxCross2 className={`${DashboardMobileStyles.cross}`} />
-                </CHFlex>
-                <CHFlex className={`${DashboardMobileStyles.selectedTags}`}>
-                  <p className={`${DashboardMobileStyles.selectTagText}`}>
-                    TAG 1
-                  </p>
-                  <RxCross2 className={`${DashboardMobileStyles.cross}`} />
-                </CHFlex>
-                <CHFlex className={`${DashboardMobileStyles.selectedTags}`}>
-                  <p className={`${DashboardMobileStyles.selectTagText}`}>
-                    TAG 1
-                  </p>
-                  <RxCross2 className={`${DashboardMobileStyles.cross}`} />
-                </CHFlex>
-              </CHFlex>
-            </li>
+                  <div className={`${DashboardMobileStyles.custom_select_Div}`}>
+                    <select className={`${DashboardMobileStyles.selectTag}`}>
+                      {item["select tags"].split(" ").map((selectItem: any) => (
+                        <option value={selectItem}>{selectItem}</option>
+                      ))}
+                    </select>
+                  </div>
+                  <CHFlex className={`${DashboardMobileStyles.selectedTagDiv}`}>
+                    <CHFlex className={`${DashboardMobileStyles.selectedTags}`}>
+                      <p className={`${DashboardMobileStyles.selectTagText}`}>
+                        TAG 1
+                      </p>
+                      <RxCross2 className={`${DashboardMobileStyles.cross}`} />
+                    </CHFlex>
+                    <CHFlex className={`${DashboardMobileStyles.selectedTags}`}>
+                      <p className={`${DashboardMobileStyles.selectTagText}`}>
+                        TAG 1
+                      </p>
+                      <RxCross2 className={`${DashboardMobileStyles.cross}`} />
+                    </CHFlex>
+                    <CHFlex className={`${DashboardMobileStyles.selectedTags}`}>
+                      <p className={`${DashboardMobileStyles.selectTagText}`}>
+                        TAG 1
+                      </p>
+                      <RxCross2 className={`${DashboardMobileStyles.cross}`} />
+                    </CHFlex>
+                    <CHFlex className={`${DashboardMobileStyles.selectedTags}`}>
+                      <p className={`${DashboardMobileStyles.selectTagText}`}>
+                        TAG 1
+                      </p>
+                      <RxCross2 className={`${DashboardMobileStyles.cross}`} />
+                    </CHFlex>
+                  </CHFlex>
+                </li>
+              ))}
           </ul>
         </CVFlex>
       </CVFlex>
+      {data.length > 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              {Object.keys(data[0]).map((key) => (
+                <th key={key}>{key}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index}>
+                {Object.values(row).map((value, index) => (
+                  <td key={index}>{value}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </CVFlex>
   );
 }
